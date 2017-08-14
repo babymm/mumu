f8627db1dd48d96bd0b759d814ab0de64e50247c
baby慕慕 <lovercws@gmail.com>
2017-08-14 16:37:40 +0800

    添加mumu-oauth单点登录功能，mumu-oauth-client使用apache-shiro进行登录管理，服务端使用cas进行控制

---
+++ mumu-common/mumu-common-security/src/main/java/com/lovecws/mumu/common/security/shiro/filter/UserCasFilter.java
@@ -0,0 +1,7 @@
+package com.lovecws.mumu.common.security.shiro.filter;
+
+/**
+ * Created by Administrator on 2017/8/14/014.
+ */
+public class UserCasFilter {
+}
---
+++ mumu-common/mumu-common-security/src/main/java/com/lovecws/mumu/common/security/shiro/realm/UserCasRealm.java
@@ -0,0 +1,7 @@
+package com.lovecws.mumu.common.security.shiro.realm;
+
+/**
+ * Created by Administrator on 2017/8/14/014.
+ */
+public class UserCasRealm {
+}
